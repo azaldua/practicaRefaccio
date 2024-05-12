69c1be59ae7aa9a70eb5baa390282b7426b2b380
adrian2bueno <113520848+adrian2bueno@users.noreply.github.com>
2024-05-12 19:48:16 +0200

    Creat classe human

---
+++ practica_REFAC/src/Human.java
@@ -0,0 +1,33 @@
+//REFACT 5
+public  class Human {
+    private String name;
+    private String age;
+    private String country;
+    private String city;
+    private String street;
+    private String house;
+    private String quarter;
+    public String obtenirAdrecaCompleta() {
+        StringBuilder result = new StringBuilder();
+        //REFACT 6:  Inline Temp
+/*        return result
+                .append(country)
+                .append(", ")
+                .append(city)
+                .append(", ")
+                .append(street)
+                .append(", ")
+                .append(house)
+                .append(" ")
+                .append(quarter).toString();*/
+        return  country +
+                ", " +
+                city +
+                ", " +
+                street +
+                ", " +
+                house +
+                " " +
+                quarter;
+    }
+}
